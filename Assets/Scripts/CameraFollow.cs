20ed6164937178ac70a4a5efe5efc8b2d4fe44a3
JabenB53 <jaben53@gmail.com>
2021-10-25 11:35:22 -0400

    Levels Complete
    Finished making all of the levels, now I just need to add menus, saves, and make it look pretty!

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -6,7 +6,8 @@ public class CameraFollow : MonoBehaviour
 {
     public Transform character; // the character's position
     public Vector3 offset = new Vector3(0,2,-10); // camera's offset from the character
-    private Vector3 newPosition; // camera's position relative to the character
+    //private Vector3 newPosition; // camera's position relative to the character
+    //public Vector3 zoomOut = new Vector3(10, 10, -20); //the zoomed out position of the camera
 
 
     // Start is called before the first frame update
@@ -19,8 +20,13 @@ public class CameraFollow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        newPosition = character.position + offset; // get the position of the character + the offset
-        transform.position = newPosition; // go to the where the character is
+        //newPosition = character.position + offset; // get the position of the character + the offset
+        //transform.position = newPosition; // go to the where the character is
+        //if (Input.GetKey(KeyCode.M))
+            //transform.position = zoomOut;
+        //else
+            transform.position = character.position + offset;
+
         //transform.LookAt(character); // camera stays fixed on the character
     }
 
