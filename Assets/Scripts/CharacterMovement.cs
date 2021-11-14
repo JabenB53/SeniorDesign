d5eb9cf91acef6170585b237e5f2b43c56f7e5ce
JabenB53 <jaben53@gmail.com>
2021-11-14 18:45:55 -0500

    Post-First-Build
    Did the first few builds, changed character movement on Ladders and made everything frictionless.

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -12,7 +12,8 @@ public class CharacterMovement : MonoBehaviour
     Rigidbody body; // physics component
     public bool touchingLadder = false; // set if the player is touching a ladder
     public int keys = 0;
-    public bool dead = false; 
+    public bool dead = false;
+    private bool counterFallingForce = true; // tracks if the player's falling force has been countered
 
     // Start is called before the first frame update
     void Start()
@@ -24,9 +25,6 @@ public class CharacterMovement : MonoBehaviour
     void Update()
     {
         XInput = Input.GetAxis("Horizontal"); // get input from the user (left or right)
-
-        //transform.Translate(Vector3.right * Time.deltaTime * speed * XInput); // move the character left or right
-        //body.AddForce(XInput * speed, 0, 0, ForceMode.Acceleration); // apply an upward force to the character
 
         if (Input.GetKeyDown(KeyCode.Space) && !touchingLadder) // if the user presses the Space key and isn't on a ladder
         {
@@ -40,20 +38,23 @@ public class CharacterMovement : MonoBehaviour
             }
         }
 
-        if (Input.GetKey(KeyCode.W)) // if the player presses 'W'
+        if (body.velocity.y >= 0) // if the player's not falling (if their falling force has been countered)
+            counterFallingForce = true;
+        if (Input.GetKey(KeyCode.W) && touchingLadder) // if the player holds 'W' AND is touching a ladder
         {
-            if (touchingLadder) // if the player is touching a ladder
+            body.useGravity = false; // disable gravity while climbing
+            if (body.velocity.y < 0 && counterFallingForce) // if the Player is falling and their falling force hasn't been countered
             {
-                body.useGravity = false; // disable gravity while climbing
-                if (body.velocity.y <= 0) // if the Player is falling
-                    body.AddForce(Vector3.up * -body.velocity.y, ForceMode.Force); // counteract that force
-                transform.Translate(Vector3.up * Time.deltaTime * speed); // move them up
+                body.AddForce(Vector3.up * -body.velocity.y, ForceMode.VelocityChange); // counteract that force
+                counterFallingForce = false;
             }
-
+            transform.Translate(Vector3.up * Time.deltaTime * speed); // move them up
         }
+
+        if (!touchingLadder || !(Input.GetKey(KeyCode.W))) // if the player is not touching a ladder OR they're not holding up
             body.useGravity = true;
 
-        if (body.velocity.y < 0)
+        if ((body.velocity.y < 0) && !touchingLadder) // while the player is falling and not touching a ladder
             body.velocity += Vector3.up * -10 * Time.deltaTime; // makes it fall faster
 
         if (dead) // once the player dies
@@ -61,28 +62,20 @@ public class CharacterMovement : MonoBehaviour
             if (Input.anyKeyDown) // wait for a key to be pressed
                 restart();    //restart the level
         }
-        
-        /*if (Input.GetKeyDown(KeyCode.Space))
-        {
-            x = 0; //number of times that jump force has been added
-            while (Input.GetKey(KeyCode.Space) && x < jumpLength)
-            {
-                body.AddForce(Vector3.up * jumpForce, ForceMode.Impulse); // add to the jump force
-                x++;
-            }
-            body.AddForce(Vector3.down * 8, ForceMode.Impulse);
-        }*/
     }
 
     private void FixedUpdate()
     {
         transform.Translate(Vector3.right * Time.deltaTime * speed * XInput); // move the character left or right
+        //body.MovePosition(transform.position + Vector3.right * Time.deltaTime * speed * XInput); // move the character left or right
     }
 
     void OnTriggerEnter(Collider other) // checks if the Player is touching certain objects
     {
         if (other.tag == "Ladder")
+        {
             touchingLadder = true; // note that the Player is touching a ladder
+        }
         if (other.tag == "MovingPlatform")
             transform.parent = other.transform; // make the platform the parent of the Player
         if (other.tag == "Key")
@@ -95,17 +88,14 @@ public class CharacterMovement : MonoBehaviour
             dead = true;
             Time.timeScale = 0; //pause the game
         }
-        /*if (other.tag == "buff")
-        {
-            jumps++;
-            Destroy(other.gameObject);
-        }*/
     }
 
     void OnTriggerExit(Collider other) // checks if the Player stops touching certain objects
     {
         if (other.tag == "Ladder")
+        {
             touchingLadder = false;
+        }
         if (other.tag == "MovingPlatform")
             transform.parent = null; // make the platform the parent of the Player
     }
@@ -116,4 +106,20 @@ public class CharacterMovement : MonoBehaviour
         Time.timeScale = 1;
     }
 
+
+
+/*    if (Input.GetKey(KeyCode.W) && touchingLadder) // if the player presses 'W' AND is touching a ladder
+        {
+                body.useGravity = false; // disable gravity while climbing
+                if (body.velocity.y< 0) // if the Player is falling
+                    body.AddForce(Vector3.up* -body.velocity.y, ForceMode.Force); // counteract that force
+                transform.Translate(Vector3.up* Time.deltaTime* speed); // move them up
+        }
+
+        if (!touchingLadder || !(Input.GetKey(KeyCode.W))) // if the player is not touching a ladder OR they're not holding up
+            body.useGravity = true;
+
+        if ((body.velocity.y < 0) && !touchingLadder) // while the player is falling and not touching a ladder
+            body.velocity += Vector3.up * -10 * Time.deltaTime; // makes it fall faster
+*/
 }
